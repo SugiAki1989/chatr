bb1361a0a17620363f0c7ee464a602480001333b
SugiAki1989 <teruaki.sugiura@gmail.com>
2020-12-10 01:38:44 +0900

    add utils

---
+++ R/utils.R
@@ -0,0 +1,23 @@
+#' Chack file size
+#' @description This function is used to chack file size.
+#' @param file_path file path of the image or video file you want to upload
+#' @examples
+#' is_file_limit(file_path = ~/path_to_file)
+#' @import
+#' @export
+
+is_file_limit <- function(file_path){
+
+  is_file_path <- file.exists(file_path)
+  if (is_file_path != TRUE) {
+    stop("image file not found. please confirm file path.")
+  }
+
+  file_size <- file.info(file_path)$size
+  if (5242880 > file_size){
+    result <- TRUE
+  } else {
+    stop("The file size exceeds the upper limit of 5MB.")
+  }
+  return(result)
+}
